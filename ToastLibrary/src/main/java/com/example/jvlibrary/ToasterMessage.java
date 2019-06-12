4f3e77ba27b4100eb2f673b0a4164414af240068
kanagalingam <kanag.jothilingam@npcompete.net>
2019-06-12 19:18:25 +0530

    Initial Commit

---
+++ ToastLibrary/src/main/java/com/example/jvlibrary/ToasterMessage.java
@@ -0,0 +1,11 @@
+package com.example.jvlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/com/example/javateacher/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.javateacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private int a;
+    private int b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
+
